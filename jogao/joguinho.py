cb301c107c2201add4b7541ac4949d1029c0f6e1
unknown <barbaramd@al.insper.edu.br>
2019-11-08 20:35:35 -0300

    Classes novas: uma para os tiros e a outra para a barra de vida (bullet e heal respectivamente).

--- jogao/joguinho.py
+++ jogao/joguinho.py
@@ -81,3 +81,66 @@ class Player(pygame.sprite.Sprite):
             self.rect.left = 0
         if self.rect.right > WIDTH / 2 - 30:
             self.rect.right =  WIDTH / 2 - 30
+            
+# Classe Bullet que representa os tiros
+class Bullet(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def _init_(self, x, y):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite._init_(self)
+        
+        # Carregando a imagem de fundo.
+        bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
+        self.image = bullet_img
+
+        self.image = pygame.transform.scale(bullet_img, (70, 30))
+
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Coloca no lugar inicial definido em x, y do constutor
+        self.rect.bottom = y + 80
+        self.rect.centerx = x + 60
+        self.speedx = 10
+
+    # Metodo que atualiza a posição da navinha
+    def update(self):
+        self.rect.x += self.speedx
+        
+        # Se o tiro passar do inicio da tela, morre.
+        if self.rect.left > WIDTH:
+            self.kill()        
+
+#clasee que cria as curas
+class Heal(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def _init_(self):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite._init_(self)
+        
+        # Carregando a imagem de fundo.
+        heal_img = pygame.image.load(path.join(img_dir, "medicine.png")).convert()
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(heal_img, (40, 40))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(WHITE)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Sorteia um lugar inicial em x
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        # Sorteia um lugar inicial em y
+        self.rect.y = random.randrange(-100, -40)
+        # Sorteia uma velocidade inicial
+        self.speedx = random.randrange(-3, 3)
+        self.speedy = random.randrange(2, 9)
