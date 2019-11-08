592c1a21d8214252bf225b02d3b8d821b77d5693
Daniel Delattre <daniieeldelattre@gmail.com>
2019-11-08 20:49:55 -0300

    class p2 e mobs

--- jogao/joguinho.py
+++ jogao/joguinho.py
@@ -115,7 +115,7 @@ class Bullet(pygame.sprite.Sprite):
         if self.rect.left > WIDTH:
             self.kill()        
 
-#clasee que cria as curas
+#clase que cria as curas
 class Heal(pygame.sprite.Sprite):
     
     # Construtor da classe.
@@ -143,3 +143,102 @@ class Heal(pygame.sprite.Sprite):
         # Sorteia uma velocidade inicial
         self.speedx = random.randrange(-3, 3)
         self.speedy = random.randrange(2, 9)
+
+#classe cria p2 
+class Player2(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def __init__(self):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        player_img = pygame.image.load(path.join(img_dir, "p2.png")).convert()
+        self.image = player_img
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(player_img, (90, 90))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(WHITE)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Centraliza embaixo da tela.
+        self.rect.centerx = 100
+        self.rect.bottom = HEIGHT / 2
+
+        # Velocidade da nave
+        self.speedx = 0
+        self.speedy = 0
+
+        # Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = 25
+
+        #health stats
+        self.health = 100
+
+        #contador da cura
+        self.cont_vida = 0      
+
+        #dano tiro p2 
+        self.damage = 1  
+     
+    
+    # Metodo que atualiza a posição da navinha
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        
+        # Mantem dentro da tela0
+        if self.rect.bottom > 570:
+            self.rect.bottom = 570
+        if self.rect.top < -28:
+            self.rect.top = -28
+
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < WIDTH / 2 + 30:
+            self.rect.left =  WIDTH / 2 + 30                                  
+
+class Meteor(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def __init__(self):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        meteor_img = pygame.image.load(path.join(img_dir, "meteor.png")).convert()
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(meteor_img, (45, 45))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Sorteia um lugar inicial em xa
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        # Sorteia um lugar inicial em y
+        self.rect.y = random.randrange(-100, -40)
+        # Sorteia uma velocidade inicial
+        self.speedx = random.randrange(-3, 3)
+        self.speedy = random.randrange(2, 9)
+        
+    # Metodo que atualiza a posição da navinha
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        
+        # Se o meteoro passar do final da tela, volta para cima
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedx = random.randrange(-3, 3)
+            self.speedy = random.randrange(2, 9)         
